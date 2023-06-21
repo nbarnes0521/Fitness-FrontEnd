fd989e46b9a2f39fffb675a0008810430502f14b
Nick <Nbarnes0521@gmail.com>
2023-06-21 17:57:45 -0500

    useState

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 export { default as App } from './Tabs';
 
@@ -6,15 +7,22 @@ export { default as App } from './Tabs';
 // Login / NavBar ///
 
 export default function App() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    console.log(username, password)
     return (
         <form>
+            <h3> Register  </h3>
             <input 
                 type="text"
                 placeholder='Enter Username'
+                onChange={(event) => setUsername(event.target.value)}
                 />
             <input 
                 type="password"
                 placeholder='Enter Password'
+                onChange={(event) => setPassword(event.target.value)}
                 />
             <button type='submit'>
                 Submit
