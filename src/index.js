bf547c155956eabed457c75579cc44bb15d80e1c
Nick <Nbarnes0521@gmail.com>
2023-06-21 01:10:21 -0500

    2nd front

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom';
+
 import  App  from './components';
+import Login from './components/Login';
 
 
 const BASE_URL = `http://fitnesstrac-kr.herokuapp.com/api`
@@ -7,6 +9,9 @@ const BASE_URL = `http://fitnesstrac-kr.herokuapp.com/api`
 const root = createRoot(document.querySelector('#app'));
 
 root.render(
+    <div> 
+        <Login />
 <App/>
+</div>
 );
 
